5d8f0a08d7a3cbe77e12217eacf48b37757a2826
Koda <sbevesays@gmail.com>
2025-11-09 09:14:02 -0800

    testing bot with 1 variable change

--- java/ModuleInterface.java
+++ java/ModuleInterface.java
@@ -1,4 +1,6 @@
 // ModuleInterface - An interface for module objects/classes
 public interface ModuleInterface {
     //variables/functions every module should have, like ButtonQty or Axes (0,1,2)
+    public int ButtonQty();
+
 }
