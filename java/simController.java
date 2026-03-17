3643cb933755f9f7c95fcc37699646c4a59cbd00
LFLOOFA <wowitsactuallyallen@gmail.com>
2026-03-17 11:16:53 -0700

    simulated Nothing class, map storage class, several functions from real firmware declared, implementation still underway

--- java/simController.java
+++ java/simController.java
@@ -2,10 +2,11 @@ public class simController {
 
 
 
+
     char[][] mapping = new char[2][4];
 
-    simCore0 internalIO;
-    simCore1 externalIO;
+    simCore0 externalIO;
+    simCore1 internalIO;
 
     public void init(){
         mapping[1][0] = 'J';
@@ -17,7 +18,17 @@ public class simController {
 
     public void simulate(){
         internalIO.simulate();
+        mapping[0]=internalIO.getPartMap();
+        externalIO.set
         externalIO.simulate();
+    }
+
+    public void setRx(byte[] in){
+        externalIO.setRx(in);
+    }
+
+    public byte[] getTx(){
+        return externalIO.getTx();
     }
 
 }
