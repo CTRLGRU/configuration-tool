8e98293a735c6778c84f3ee0f3690b1e0848087c
LFLOOFA <wowitsactuallyallen@gmail.com>
2026-03-18 13:50:41 -0700

    translation functions

--- java/simCore0.java
+++ java/simCore0.java
@@ -79,11 +79,36 @@ public class simCore0 implements simCore{
     }
 
     private void translateButtonToJoystick(int line){
-
+        boolean up = ((workBuffer[line][0] >> 3) & 1) == 1;
+        boolean down = ((workBuffer[line][0] >> 2) & 1) == 1;
+        boolean left = ((workBuffer[line][0] >> 1) & 1) == 1;
+        boolean right = ((workBuffer[line][0]) & 1) == 1;
+        short xAxis = (short) (512 + (right ? 1: 0) * 512 - (left ? 1 : 0) * 512);
+        short yAxis = (short) (512 + (up ? 1: 0) * 512 - (down ? 1 : 0) * 512);
+        workBuffer[line][0] = (byte) (xAxis >> 8);
+        workBuffer[line][1] = (byte) xAxis;
+        workBuffer[line][2] = (byte) (yAxis >> 8);
+        workBuffer[line][3] = (byte) yAxis;
     }
 
     private void translateJoystickToButton(int line){
+        int xval = ((workBuffer[line][0] & 0x3F) << 8);
+        xval |= workBuffer[line][1];
+        int yval = (workBuffer[line][2] << 8);
+        yval |= workBuffer[line][3];
 
+        workBuffer[line][0] = 0;
+
+        if (xval > 768) {
+            workBuffer[line][0] |= (1 << 3);
+        } else if (xval < 256) {
+            workBuffer[line][0] |= (1 << 2);
+        }
+        if (yval < 256) {
+            workBuffer[line][0] |= (1 << 1);
+        } else if (xval > 768) {
+            workBuffer[line][0] |= (1);
+        }
     }
 
     private void output(){
